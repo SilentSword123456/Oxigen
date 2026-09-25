555969d2c940524965523c9529210e4a64848c61
SilentSword <andrei925.dumitru@gmail.com>
2026-09-25 21:56:39 +0300

    Added double jump

--- player.gd
+++ player.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 	
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
+var JUMP_NUMBER=0
 
 func _chose_animation() -> void:
 	if is_zero_approx(velocity.x) && is_zero_approx(velocity.y):
@@ -13,7 +14,10 @@ func _chose_animation() -> void:
 		if velocity.y>0:
 			$AnimatedSprite2D.play("Fall")
 		else:
-			$AnimatedSprite2D.play("Jump")
+			if JUMP_NUMBER==1:
+				$AnimatedSprite2D.play("Double Jump")
+			else:
+				$AnimatedSprite2D.play("Jump")
 	else:
 		$AnimatedSprite2D.play("Run")
 		$AnimatedSprite2D.flip_h = velocity.x < 0
@@ -24,8 +28,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || JUMP_NUMBER<2):
 		velocity.y = JUMP_VELOCITY
+		JUMP_NUMBER += 1
+		
+	if is_on_floor():
+		JUMP_NUMBER=0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
